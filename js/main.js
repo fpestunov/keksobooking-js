75463db53bf7dc39c69fb459185604aa184f96c3
fpestunov <fpestunov@gmail.com>
2020-02-12 11:09:59 +0400

    add pin state

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 "use strict";
 
 var PIN = {
-  WIDTH: 50,
-  HEIGHT: 70
+  WIDTH: 62,
+  HEIGHT: 84
+};
+var APPLICATION_WIDTH = {
+  MIN: 1,
+  MAX: 1200
+};
+var APPLICATION_HEIGHT = {
+  MIN: 130,
+  MAX: 630
 };
 var FILE_PATH = "img/avatars/user0";
 var FILE_FORMAT = ".png";
@@ -10,32 +18,31 @@ var TITLE_MIN_LENGTH = 30;
 var TITLE_MAX_LENGTH = 100;
 var MAX_PRICE_PER_NIGHT = 1000000;
 
-var mainPin = document.querySelector(".map__pin--main");
-var map = document.querySelector(".map");
-var form = document.querySelector(".notice");
-var adForm = form.querySelector(".ad-form");
-var adFormFields = form.querySelectorAll("input");
-var adFormSelects = form.querySelectorAll("select");
-var adFormFieldsets = form.querySelectorAll("fieldset");
-var filterForm = form.querySelector(".map__filters");
-var headlineField = form.querySelector("#title");
-var perNightField = form.querySelector("#price");
-var choiceOfHousingType = form.querySelector("#type");
-var addressField = form.querySelector("#address");
 var houseTypeByPrice = {
   bungalo: 0,
   flat: 1000,
   house: 5000,
   palace: 10000
 };
-var appartmentType = {
+var AppartmentType = {
   BUNGALO: "bungalo",
   FLAT: "flat",
   HOUSE: "house",
   PALACE: "palace"
 };
+var mainPin = document.querySelector(".map__pin--main");
+var map = document.querySelector(".map");
+var form = document.querySelector(".ad-form");
+var formFields = document.querySelectorAll("input");
+var formSelects = document.querySelectorAll("select");
+var adFormFieldsets = form.querySelectorAll("fieldset");
+var headlineField = form.querySelector("#title");
+var perNightField = form.querySelector("#price");
+var choiceOfHousingType = form.querySelector("#type");
+var addressField = form.querySelector("#address");
 var arriveTimeField = form.querySelector("#timein");
 var departureTimeField = form.querySelector("#timeout");
+var activatePin = false;
 var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
@@ -102,23 +109,78 @@ var makeFiledFragment = function(pins) {
 };
 
 var fragment = makeFiledFragment(getPins(8));
-var getFormElements = function(formElements, state) {
+var getFormElements = function(formElements, state, pointer) {
   formElements.forEach(function(element) {
     element.disabled = state;
+    element.style = pointer;
   });
 };
-getFormElements(adFormFieldsets, true);
+getFormElements(formFields, true, "pointer-events: none");
+getFormElements(formSelects, true, "pointer-events: none");
+getFormElements(adFormFieldsets, true, "pointer-events: none");
 var onPinClick = function() {
   similarListAds.appendChild(fragment);
   map.classList.remove("map--faded");
-  adForm.classList.remove("ad-form--disabled");
-  getFormElements(adFormFields, false);
-  getFormElements(adFormSelects, false);
-  getFormElements(adFormFieldsets, false);
-  filterForm.classList.remove("map__filters-container");
+  form.classList.remove("ad-form--disabled");
+  getFormElements(formFields, false, false);
+  getFormElements(formSelects, false, false);
+  getFormElements(adFormFieldsets, false, false);
+  activatePin = true;
 };
 
 mainPin.addEventListener("click", onPinClick);
+mainPin.addEventListener("mousedown", function(evt) {
+  evt.preventDefault();
+
+  if (map.classList.contains("map--faded")) {
+    onPinClick();
+    mainPin.removeEventListener("click", onPinClick);
+  }
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function(moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    var currentCoordY = mainPin.offsetTop - shift.y;
+    var currentCoordX = mainPin.offsetLeft - shift.x;
+
+    if (
+      currentCoordY >= APPLICATION_HEIGHT.MIN &&
+      currentCoordY <= APPLICATION_HEIGHT.MAX
+    ) {
+      mainPin.style.top = currentCoordY + "px";
+    }
+    if (
+      currentCoordX >= APPLICATION_WIDTH.MIN &&
+      currentCoordX <= APPLICATION_WIDTH.MAX - PIN.WIDTH
+    ) {
+      mainPin.style.left = currentCoordX + "px";
+    }
+    addressField.value =
+      mainPin.offsetLeft + PIN.WIDTH / 2 + ", " + mainPin.offsetTop;
+  };
+  var onMouseUp = function(upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  };
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+});
 
 headlineField.minLength = TITLE_MIN_LENGTH;
 headlineField.maxLength = TITLE_MAX_LENGTH;
@@ -129,19 +191,19 @@ perNightField.required = true;
 
 var changeMinPrice = function() {
   switch (choiceOfHousingType.value) {
-    case appartmentType.BUNGALO:
+    case AppartmentType.BUNGALO:
       perNightField.min = houseTypeByPrice.bungalo;
       perNightField.placeholder = houseTypeByPrice.bungalo;
       break;
-    case appartmentType.FLAT:
+    case AppartmentType.FLAT:
       perNightField.min = houseTypeByPrice.flat;
       perNightField.placeholder = houseTypeByPrice.flat;
       break;
-    case appartmentType.HOUSE:
+    case AppartmentType.HOUSE:
       perNightField.min = houseTypeByPrice.house;
       perNightField.placeholder = houseTypeByPrice.house;
       break;
-    case appartmentType.PALACE:
+    case AppartmentType.PALACE:
       perNightField.min = houseTypeByPrice.palace;
       perNightField.placeholder = houseTypeByPrice.palace;
       break;
